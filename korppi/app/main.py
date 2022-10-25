41d063c67ead607d9c975fb6155cc38e519f27d0
aepyli <115064561+aepyli@users.noreply.github.com>
2022-10-25 13:30:01 +0300

    modified the functions & added print

--- korppi/app/main.py
+++ korppi/app/main.py
@@ -17,8 +17,6 @@ System description:
 
 speed_ranges =  [0, 1, 2, 3, 4, 5] # List contains 6 values which are counted from 0 to 5
 
-#print(speed_ranges[5]) #[0..5] but we have 6 values
-
 # a function to get actual speed limits in km/h.
 def get_speed_limits(range):
   match range:
@@ -28,42 +26,11 @@ def get_speed_limits(range):
     case 3: return 80
     case 4: return 100
     case 5: return 120
-  
-def get_speedlimit(spdlim):
-  return spdlim
 
-# Create a function which simulates speed limits
-def set_speed_limit(somerange):
-    match somerange:
-        case 0:
-            #print(" I ha identified this case")
-            return random.randint(0, 30)
-        case 1:
-            return random.randint(30, 50)
-        case 2:
-            return random.randint(50, 60)
-        case 3:
-            return random.randint(60, 80)
-        case 4:
-            return random.randint(80, 100)
-        case 5:
-            return random.randint(100, 120)
-    
-# The variable used by the function 
-#in_var = "I am the variable value"
+def get_speedlimit(spdlimit):
+  return get_speed_limits(spdlimit)
 
-# Excecute the function using print
 
-ctr = 0 # A counter
+for i in range (0, 6):
+  print(get_speedlimit(i))
 
-# A while loop which could run forever if the counter and IF condition would not be defined
-while  True: 
-    random_speed_rng_gen = random.randint(0, 5)
-    print("Current speed range -> ", random_speed_rng_gen)
-    #print(set_speed_limit()
-    
-    ctr += 1  # I increment the counter by 1
-    
-    # if condition. Breaks <<while loop>> if ctr == 100
-    if ctr == 100:
-        break
