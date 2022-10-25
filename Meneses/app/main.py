ab0037aa85b00d2331d700a2929a2fb01fad147b
daniel meneses <daniel.meneses@osaango.com>
2022-10-25 08:30:54 +0300

    some coding in python

---
+++ Meneses/app/main.py
@@ -0,0 +1,31 @@
+import random
+
+'''
+System description:
+* streets have signs to make drivers aware about the speed limit. 
+    If I have a LIST of speed ranges then I would use a python list.
+
+    Speed range are: []
+    * 0 [0..30]km/h
+    * 1 [30..50]km/h
+    * 2 [50..60]km/h
+    * 3 [60..80]km/h
+    * 4 [80..100]km/h
+    * 5 [100..120]km/h
+
+'''
+
+speed_ranges =  [0, 1, 2, 3, 4, 5] # List contains 6 values which are counted from 0 to 5
+
+#print(speed_ranges[5]) #[0..5] but we have 6 values
+
+# Create a function which simulates speed limits
+def set_speed_limit(in_var):
+
+    return in_var
+
+# The variable used by the function 
+in_var = "I am the variable value"
+
+# Excecute the function using print
+print(set_speed_limit(in_var))
