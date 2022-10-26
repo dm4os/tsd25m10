3c5d2d380faec3202d2f39fa9b8ad94a90c0ab0a
Satu Eskelinen <s.eskelinen@gmail.com>
2022-10-26 09:32:27 +0300

    Added comments and specification, trying again

--- Eskelinen/app/ex_speedlimit.py
+++ Eskelinen/app/ex_speedlimit.py
@@ -1,6 +1,23 @@
 import random
 
 '''
+# Speed controller of autonomous vehicles
+
+## Behavior-driven development [BDD] approach  
+As a        driver
+I WANT      the car to determine the speed limit and adjust the speed to that limit.
+SO THAT     I don't have to adjust the speed myself
+
+## Acceptance test-driven development [ATDD] approach 
+GIVEN       the speed limit 70km/h
+WHEN        the car in moving
+THEN        the speed limit should remain/adjusted between 65 and 70km/h
+
+GIVEN       the speed limit 70km/h
+WHEN        the speed limit changes to 50km/h
+THEN        deceleration rate should be 15km/h
+
+
 System description:
 * streets have signs to make drivers aware about the speed limit. 
     If I have a LIST of speed ranges then I would use a python list.
